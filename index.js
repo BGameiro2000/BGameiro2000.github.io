f11cddc6fa8e3351391ae5eace4c3f0a35fb8291
Unknown <bernardo.gameiro.0.1@gmail.com>
2019-08-30 00:50:39 +0000

    info now comes from JSON file

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var DoB = "6/9/2000" // MM/DD/YYYY
+var request = new XMLHttpRequest();
+request.open("GET", "./index.json", false);
+request.send(null);
+var myJSON = JSON.parse(request.responseText);
+alert (myJSON.result[0]);
 
-function myAge( ){
+function myAge(myDate){
     var today = new Date();
-    var birthDate = new Date(DoB);
+    var birthDate = new Date(myDate);
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())){
         age--;
     }
     return age;
+}
+
+function aboutPlus(){
+    for (title in myJSON.author.about.other){
+        document.write("<hr>");
+        document.write("<h3>"+title+":</h3>");
+        for (var i = 0; i < title.lenght; i++){
+            var obj = title[i];
+            document.write("<p>"+obj+"</p>");
+        }
+    }
 }
